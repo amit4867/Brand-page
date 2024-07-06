87a809fc91c22f38ae78b0c7a5c5034d79d1333b
Amit Kumar Baral <152974735+amit4867@users.noreply.github.com>
2024-07-06 22:22:47 +0530

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./componets/Navigation";
+import HeroSection from "./componets/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
